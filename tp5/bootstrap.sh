fd00a09e73c14d9d45c45b2ca4e7bee924dffefb
EC2 Default User <ec2-user@ip-10-42-42-208.eu-west-3.compute.internal>
2025-03-20 15:01:08 +0000

    correction script user data

--- tp5/bootstrap.sh
+++ tp5/bootstrap.sh
@@ -1,7 +1,18 @@
-#!/bin/bash
-sudo yum -y install amazon-efs-utils
-sudo yum -y install python3-pip
-sudo pip3 install botocore --upgrade
-sudo mkdir /mnt/efs
-fs_id="${fs_id}"
-sudo mount -t efs -o tls $fs_id:/ /mnt/efs
+#!/bin/bash -x
+
+export DEBIAN_FRONTEND=noninteractive
+
+# Update the package repository
+sudo apt update
+
+# Install necessary packages
+sudo apt install -y apache2 awscli libapache2-mod-php mysql-client nfs-common php php-cli php-common php-curl php-fpm php-gd php-imap php-mbstring php-mysql php-redis php-xml php-zip unzip
+
+# Create the mount point for the EFS file system
+sudo mkdir -p /mnt/efs
+
+# Mount the EFS file system
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${efs_dns}:/ /mnt/efs
+
+# Add the EFS mount to /etc/fstab to mount it automatically at boot
+echo '${efs_dns}:/ /mnt/efs nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport,_netdev 0 0' | sudo tee -a /etc/fstab
